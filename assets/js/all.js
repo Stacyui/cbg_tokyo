dc70175b5040098de5e2335176e6780b56c3b673
yui.fujikawa <yui.fujikawa@micro-wave.net>
2023-05-11 17:56:22 +0900

    initial commit

---
+++ assets/js/all.js
@@ -0,0 +1,19 @@
+$(function () {
+    $(window).on('scroll', function () {
+        if ($('.mv').height()  < $(this).scrollTop()) {
+            $('.js-scrollHeader').addClass('change-color');
+        } else {
+            $('.js-scrollHeader').removeClass('change-color');
+        }
+
+        if ($('.mv').height()  < $(this).scrollTop()) {
+            $('.link-item').addClass('text-color');
+        } else {
+            $('.link-item').removeClass('text-color');
+        }
+    });
+});
+
+$(".openbtn1").click(function () {
+    $(this).toggleClass('active');
+});
